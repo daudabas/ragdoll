a4c75a9ad835ab4c53bca0315ce709cc689aa7f2
Daud Abas <daud.abas@2359media.com>
2018-12-23 20:05:53 +0800

    Fix transparent background for chrome

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,7 +3,7 @@ import Game from './game';
 import Modal from 'react-modal';
 import Info from '../info';
 import Face from '../face';
-import audioFile from './sounds/audio.mp3';
+// import audioFile from './sounds/audio.mp3';
 
 Modal.setAppElement('#root');
 
@@ -122,7 +122,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <div>
-        <audio src={audioFile} autoPlay/>
+        {/* <audio src={audioFile} autoPlay/> */}
         <Game ref={this.gameRef} headURL={this.state.headURL} headCallback={this.showGreetings.bind(this)}/>
         <div className="buttons">
           <div className="left-buttons">
